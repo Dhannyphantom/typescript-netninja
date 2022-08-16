643dff8da06263e2af07f9b9f30078df2f669da5
Dhannyphantom <youngskillzzz@gmail.com>
2022-08-17 00:01:29 +0100

    Tried out some basic typescript type rules

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+var car = {
+    name: "toyota",
+    model: "camry",
+    year: 2022,
+    hasAI: true
+};
+car.name = "mini cooper";
+car.year = 2023;
+var age;
+var bool;
+var namer;
+var year;
+var people = [];
+var mixed = [];
+mixed.push(4);
+mixed.push("dan");
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,21 @@
-const myName = "Daniel";
+let car = {
+  name: "toyota",
+  model: "camry",
+  year: 2022,
+  hasAI: true,
+};
 
-console.log("Hello " + myName);
+car.name = "mini cooper";
+car.year = 2023;
+
+let age: number;
+let bool: boolean;
+let namer: string;
+let year: number;
+
+let people: string[] = [];
+
+let mixed: (string | number)[] = [];
+
+mixed.push(4);
+mixed.push("dan");
