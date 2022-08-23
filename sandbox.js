6961e5def51acea6b9dd5666df4c18170fec4bf5
Dhannyphantom <youngskillzzz@gmail.com>
2022-08-23 22:52:54 +0100

    Explicit & Union type --mine

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,24 @@
-var btn = document.querySelector("button");
-var selector = document.querySelector("select");
-var title = document.querySelector("h1");
-var counter = 16;
-btn.onclick = function () {
-    //   console.log(selector.value);
-    counter += 4;
-    title.style.fontSize = "".concat(counter, "px");
+// READY FOR LESSONS
+var sum = function (a, b) {
+    return a + b;
 };
+var result = sum(3, 6);
+console.log(result);
+// EXPLICIT TYPES
+var str;
+var num;
+var bool;
+// ARRAYS
+var arrNum;
+var arrStr;
+var arrObj = [];
+// let obj: object;
+var obj;
+obj = { name: "dan", age: 30 };
+// arrObj.push({name: "yoshi"});
+arrObj.push(obj);
+obj.name = "Yello";
+// UNION TYPE
+var UID;
+UID = "43fdbhfbs4";
+UID = 434;
--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,35 @@
 // READY FOR LESSONS
+let sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+const result = sum(3, 6);
+
+console.log(result);
+
+// EXPLICIT TYPES
+let str: string;
+let num: number;
+let bool: boolean;
+
+// ARRAYS
+let arrNum: number[];
+let arrStr: string[];
+
+let arrObj: { name: string; age: number }[] = [];
+
+// let obj: object;
+let obj: { name: string; age: number };
+
+obj = { name: "dan", age: 30 };
+
+// arrObj.push({name: "yoshi"});
+arrObj.push(obj);
+
+obj.name = "Yello";
+
+// UNION TYPE
+let UID: string | number;
+
+UID = "43fdbhfbs4";
+UID = 434;
